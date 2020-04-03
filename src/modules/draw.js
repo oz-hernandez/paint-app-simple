d84aaea2a6734c82222b3e4166df4832cad38901
Osmani Hernandez <ohernandez@izotope.com>
2020-04-03 17:58:05 -0400

    basic button functionality. switch logic when a button is clicked

--- src/modules/draw.js
+++ src/modules/draw.js
@@ -3,54 +3,54 @@ export function updateCanvas() {
    console.log('inside update canvas');
 }
 
-export function pencilDraw() {
+let methodsToRemove = new Map();
+
+export function draw(event) {
     let canvas = document.getElementById('canvas');
     let context = canvas.getContext('2d');
     var rect = canvas.getBoundingClientRect();
-    function draw(event) {
-        context.lineTo(event.clientX - rect.left, event.clientY - rect.top);
-        context.stroke();
-    }
-   
-    canvas.addEventListener('mousedown', event => {
-        let context = canvas.getContext('2d');
-        context.strokeStyle = 'blue';
-        context.lineWidth = 2;
-        context.beginPath();
-        context.moveTo(event.clientX - rect.left, event.clientY - rect.top);
-        canvas.addEventListener('mousemove', draw);
-    });
 
-    // var clearButton = document.getElementById('clear');
+    switch(event.target.id) {
+        case "pencil":
+            function draw(event) {
+                context.lineTo(event.clientX - rect.left, event.clientY - rect.top);
+                context.stroke();
+            }
+            function setup(event) {
+                let context = canvas.getContext('2d');
+                context.strokeStyle = 'blue';
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(event.clientX - rect.left, event.clientY - rect.top);
+                canvas.addEventListener('mousemove', draw);
+            }
+            canvas.addEventListener('mousedown', setup);
+            canvas.addEventListener('mouseup', event => {
+                canvas.removeEventListener('mousemove', draw);
+            });
+
+            if(!methodsToRemove.has("pencil")) {
+                methodsToRemove.set("pencil", setup);
+            }
+            break;
+        case "eraser":
+            canvas.removeEventListener('mousedown', methodsToRemove.get('pencil'));
+            canvas.addEventListener('mousedown', event => {
+                console.log("mouse down");
+                canvas.addEventListener('mousemove', event => {
+                    console.log("hello");
+                });
+            });
+            canvas.addEventListener('mouseup', (event) =>{
+                // canvas.removeEventListener('mousemove', draw);
+            });
+            break;
+        }
+    }
+
+
+  // var clearButton = document.getElementById('clear');
 
     // clearButton.addEventListener('click', function() {
     //   context.clearRect(0, 0, canvas.width, canvas.height);
     // });
-
-    canvas.addEventListener('mouseup', (event) => {
-        canvas.removeEventListener('mousemove', draw);
-    });
-}
-
-export function erase() {
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext('2d');
-    var rect = canvas.getBoundingClientRect();
-    function draw(event) {
-        // context.lineTo(event.clientX - rect.left, event.clientY - rect.top);
-        // context.stroke();
-    }
-   
-    canvas.addEventListener('mousedown', event => {
-        // let context = canvas.getContext('2d');
-        // context.strokeStyle = 'blue';
-        // context.lineWidth = 2;
-        // context.beginPath();
-        // context.moveTo(event.clientX - rect.left, event.clientY - rect.top);
-        // canvas.addEventListener('mousemove', draw);
-    });
-    canvas.addEventListener('mouseup', (event) =>{
-        canvas.removeEventListener('mousemove', draw);
-    });
- console.log('hello');
-}
